4e129744fce278304b3d91d090e8a5bdd0902f93
goha <gohaogorodnikov@gmail.com>
2025-06-28 16:57:56 +0300

    Реализовал объект Player.php

---
+++ Player.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+class Player
+{
+    private string $name;
+
+    private int $health;
+
+    private ?Weapon $weapon;
+
+    private Point $point;
+
+    public function __construct(string $name, int $health, Point $point)
+    {
+        $this->name = $name;
+        $this->health = $health;
+        $this->point = $point;
+    }
+
+    public function takeDamage(int $damage)
+    {
+        if ($damage > 0) {
+            $this->name -= $damage;
+        }
+    }
+
+    public function equipWeapon(Weapon $weapon)
+    {
+        $this->weapon = $weapon;
+    }
+
+    public function isDead(): bool
+    {
+        if ($this->health <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public function moveTo(Point $newPoint)
+    {
+        $this->point = $newPoint;
+    }
+
+    public function getAttackDamage(): int
+    {
+        if (empty($this->weapon)) {
+            return 1;
+        }
+
+        return $this->weapon->getDamage();
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getHealth(): int
+    {
+        return $this->health;
+    }
+
+    public function getPoint(): Point
+    {
+        return $this->point;
+    }
+}
