5c58aad57ce72f5ea96ebdf1a407e921f6677d36
goha <gohaogorodnikov@gmail.com>
2025-06-30 10:42:06 +0300

    добавил защиту в класс Point

--- src/World/Point.php
+++ src/World/Point.php
@@ -9,6 +9,9 @@ class Point
 
     public function __construct(int $x, int $y)
     {
+        if (($x < 0 || $x > 10) || ($y < 0 || $y > 10)) {
+            throw new InvalidArgumentException('Значение x или y не может быть меньше 0 или больше 10.');
+        }
         $this->x = $x;
         $this->y = $y;
     }
