bc3606b6c537a47fec33cd939af8caf27d9c92a8
goha <gohaogorodnikov@gmail.com>
2025-06-30 11:35:32 +0300

    refactor: убрал опасный __construct() добавил функцию валидатор

--- src/Abilities/TAttacker.php
+++ src/Abilities/TAttacker.php
@@ -4,19 +4,25 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../Items/Weapon.php';
 
+/**
+ * Трейт для атакующих сущностей
+ *
+ * !!! ОБЯЗАТЕЛЬНО ИСПОЛЬЗОВАТЬ ВАЛИДАЦИЮ validateInnateDamage()
+ */
 trait TAttacker
 {
     private ?Weapon $equippedWeapon;
 
     private int $innateDamage;
 
-    public function __construct(int $innateDamage, ?Weapon $weapon)
+    /**
+     * @throws InvalidArgumentException если innateDamage меньше или равен нулю
+     */
+    public function validateInnateDamage(): void
     {
-        if (isset($weapon)) {
-            $this->equippedWeapon = $weapon;
+        if ($this->innateDamage <= 0) {
+            throw new InvalidArgumentException('innateDamage не может быть меньше или равен нулю');
         }
-
-        $this->innateDamage = $innateDamage;
     }
 
     public function getAttackDamage(): int
