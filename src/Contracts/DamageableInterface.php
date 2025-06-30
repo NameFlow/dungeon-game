75ce04540f4d6c610d3aa45026bb05fd95e061ea
goha <gohaogorodnikov@gmail.com>
2025-06-30 08:41:06 +0300

    Добавил возвращаемое значение методу takeDamage(): void

--- src/Contracts/DamageableInterface.php
+++ src/Contracts/DamageableInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 interface DamageableInterface
 {
-    public function takeDamage(int $damage);
+    public function takeDamage(int $damage): void;
 
     public function isDead(): bool;
 
