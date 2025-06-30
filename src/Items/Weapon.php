e133a9d775e7b3f037cc7181348ad2d1f4b2c6a1
goha <gohaogorodnikov@gmail.com>
2025-06-30 12:06:24 +0300

    Добавил валидацию свойств в construct

--- src/Items/Weapon.php
+++ src/Items/Weapon.php
@@ -10,6 +10,12 @@ class Weapon
 
     public function __construct(string $name, int $damage)
     {
+        if (empty(trim($name))) {
+            throw new InvalidArgumentException('Свойство name не может быть пустым');
+        } elseif ($damage <= 0) {
+            throw new InvalidArgumentException('Свойство damage может быть меньше или равен 0');
+        }
+
         $this->name = $name;
         $this->damage = $damage;
     }
